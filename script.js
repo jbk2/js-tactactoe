6656ebc189bd5a8799fc6ccbec19e5c73c5a3878
jbk2 <james@humanbenefits.com>
2024-11-26 17:23:19 +0100

    Create animated svg for 'x'

---
+++ script.js
@@ -0,0 +1,41 @@
+let redX =  `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0,0 L100,100"
+                stroke="red"
+                stroke-width="5"
+                fill="none"
+                stroke-dasharray="142"
+                stroke-dashoffset="142"
+                id="stroke1"
+              />
+              <path
+                d="M100,0 L0,100"
+                stroke="red"
+                stroke-width="5"
+                fill="none"
+                stroke-dasharray="142"
+                stroke-dashoffset="142"
+                id="stroke2"
+              />
+            </svg>`;
+
+const blackX =  `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M0,0 L100,100"
+                    stroke="red"
+                    stroke-width="5"
+                    fill="none"
+                    stroke-dasharray="142"
+                    stroke-dashoffset="142"
+                    id="stroke1"
+                  />
+                  <path
+                    d="M100,0 L0,100"
+                    stroke="black"
+                    stroke-width="5"
+                    fill="none"
+                    stroke-dasharray="142"
+                    stroke-dashoffset="142"
+                    id="stroke2"
+                  />
+                </svg>`;
