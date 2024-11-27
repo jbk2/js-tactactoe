585784e9856e8d720dc88bfa9a70668fff90f6a1
jbk2 <james@humanbenefits.com>
2024-11-27 16:29:46 +0100

    build board and move scripts

--- script.js
+++ script.js
@@ -1,41 +1,37 @@
-let redX =  `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-              <path
-                d="M0,0 L100,100"
-                stroke="red"
-                stroke-width="5"
-                fill="none"
-                stroke-dasharray="142"
-                stroke-dashoffset="142"
-                id="stroke1"
-              />
-              <path
-                d="M100,0 L0,100"
-                stroke="red"
-                stroke-width="5"
-                fill="none"
-                stroke-dasharray="142"
-                stroke-dashoffset="142"
-                id="stroke2"
-              />
-            </svg>`;
+document.addEventListener('DOMContentLoaded', function() {
+  const redX    = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><path d="M0,0 L100,100" stroke="red" stroke-width="5" fill="none" stroke-dasharray="142" stroke-dashoffset="142" id="stroke1"/><path d="M100,0 L0,100" stroke="red" stroke-width="5" fill="none" stroke-dasharray="142" stroke-dashoffset="142" id="stroke2"/></svg>`;
+  const blackCircle = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="45" stroke="black" stroke-width="5" fill="none" class="animated-circle"/></svg>`;
 
-const blackX =  `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M0,0 L100,100"
-                    stroke="red"
-                    stroke-width="5"
-                    fill="none"
-                    stroke-dasharray="142"
-                    stroke-dashoffset="142"
-                    id="stroke1"
-                  />
-                  <path
-                    d="M100,0 L0,100"
-                    stroke="black"
-                    stroke-width="5"
-                    fill="none"
-                    stroke-dasharray="142"
-                    stroke-dashoffset="142"
-                    id="stroke2"
-                  />
-                </svg>`;
+  let cells = document.querySelectorAll('#board-grid > div');
+  
+  const board = function()  {
+    let boardArray = []
+    
+    function recordMove(move) {
+      boardArray.push(move);
+      console.log('Move added to board:', move);
+      console.log('Current board state:', boardArray);
+    }
+
+    function returnMoves() {
+      return [...boardArray]; 
+    }
+
+    return { recordMove, returnMoves, displayMoves };
+  }();
+
+  cells.forEach((cell) => {
+    cell.addEventListener('click', processMove);
+  })
+  
+  function processMove(event) {
+    console.log('processMove triggered for:', event.target);
+    let cellPosition = event.srcElement.dataset.cellPosition;
+    board.addMove(cellPosition);
+    board.getState;
+    event.target.insertAdjacentHTML('beforeEnd', blackCircle);
+    event.target.removeEventListener('click', processMove);
+    event.target.style.cursor = 'default';
+  }
+
+})
