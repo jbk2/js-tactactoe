d5d0d4836cb9ee85980f7558f54f31b1beb91a5e
jbk2 <james@humanbenefits.com>
2024-12-04 16:23:38 +0100

    1st stage of move to object modularity with game and ui separation

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const redX    = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><path d="M0,0 L100,100" stroke="red" stroke-width="5" fill="none" stroke-dasharray="142" stroke-dashoffset="142" id="stroke1"/><path d="M100,0 L0,100" stroke="red" stroke-width="5" fill="none" stroke-dasharray="142" stroke-dashoffset="142" id="stroke2"/></svg>`;
-  const blackCircle = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="45" stroke="black" stroke-width="5" fill="none" class="animated-circle"/></svg>`;
+  const x = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><path d="M0,0 L100,100" stroke="red" stroke-width="5" fill="none" stroke-dasharray="142" stroke-dashoffset="142" id="stroke1"/><path d="M100,0 L0,100" stroke="red" stroke-width="5" fill="none" stroke-dasharray="142" stroke-dashoffset="142" id="stroke2"/></svg>`;
+  const o = `<svg width="100" height="100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="45" stroke="black" stroke-width="5" fill="none" class="animated-circle"/></svg>`;
 
-  let cells = document.querySelectorAll('#board-grid > div');
-  
+  // board object stores, records and returns moves from boardArray array
   const board = function()  {
     let boardArray = []
     
@@ -16,20 +15,31 @@ document.addEventListener('DOMContentLoaded', function() {
       console.log('Current board state:', boardArray);
       return [...boardArray]; 
     }
-
-    function lastMoveCell() {
-      return document.querySelector(`[data-cell-position="${boardArray.at(-1)}"]`);
+    
+    function lastMove() {
+      return boardArray.at(-1)
     }
     
+    return { recordMove, returnMoves, lastMove };
+  }();
+  
+  function uiController() {
+    const cells = document.querySelectorAll('#board-grid > div');
+    const instructions = document.getElementById('instructions');
+    
+    function lastMoveCell() {
+      return document.querySelector(`[data-cell-position="${board.lastMove}"]`);
+    }
+
     function displayMove() {
       let cell = lastMoveCell();
-      cell.insertAdjacentHTML('beforeend', blackCircle);
+      cell.insertAdjacentHTML('beforeend', game.activePlayer.symbol);
       cell.removeEventListener('click', processMove);
       cell.style.cursor = 'default';
     }
 
-    return { recordMove, returnMoves, displayMove };
-  }();
+    return { displayMove }
+  }
 
   cells.forEach((cell) => {
     cell.addEventListener('click', processMove);
@@ -42,5 +52,28 @@ document.addEventListener('DOMContentLoaded', function() {
     board.returnMoves();
     board.displayMove();
   }
+
+  function createPlayer(symbol) {
+    const symbol = symbol;
+    const getSymbol = () => symbol;
+
+    return { getSymbol }; 
+  };
+
+
+  function GameController() {
+    const player1 = createPlayer('x');
+    const player2 = createPlayer('o');
+    let activePlayer = player1;
+    const changeActivePlayer = () => {
+      activePlayer = activePlayer === player1 ? player2 : player1;
+    };
+    const getActivePlayer = ()  => activePlayer;
+
+    return { getActivePlayer, changeActivePlayer };
+  };
+
+
+  const game = GameController()
 
 })
